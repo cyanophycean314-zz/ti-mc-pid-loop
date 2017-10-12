878dbeb06a8f479f772275a3d1356ce27d8f5013
cyanophycean314 <kev1fei@gmail.com>
2017-10-12 15:20:30 -0400

    working code for tuning kp

--- main.c
+++ main.c
@@ -22,8 +22,22 @@
 #pragma DATA_SECTION(analog_ins, "ramgs0");
 Uint16 analog_ins[NUM_OUTPUTS][NUM_INPUTS_PER_OUTPUT];
 
+// declare variable holding gains
+#pragma DATA_SECTION(kp, "ramsg0");
+Uint16 kp;
+
 // declare array holding DAC values
 Uint16 analog_outs[NUM_OUTPUTS];
+
+// move this code to RAM
+#pragma CODE_SECTION(dma_ch1_int_isr, ".TI.ramfunc");
+__interrupt void dma_ch1_int_isr(void)
+{
+	// acknowledge that the interrupt was received
+	EALLOW;
+	PieCtrlRegs.PIEACK.bit.ACK1 = PIEACK_GROUP7;
+	EDIS;
+}
 
 // move this code to RAM
 #pragma CODE_SECTION(dma_ch2_int_isr, ".TI.ramfunc");
@@ -45,6 +59,36 @@ __interrupt void dma_ch3_int_isr(void)
 	EDIS;
 }
 
+// move this code to RAM
+#pragma CODE_SECTION(dma_ch4_int_isr, ".TI.ramfunc");
+__interrupt void dma_ch4_int_isr(void)
+{
+	// acknowledge that the interrupt was received
+	EALLOW;
+	PieCtrlRegs.PIEACK.bit.ACK4 = PIEACK_GROUP7;
+	EDIS;
+}
+
+// move this code to RAM
+#pragma CODE_SECTION(dma_ch5_int_isr, ".TI.ramfunc");
+__interrupt void dma_ch5_int_isr(void)
+{
+	// acknowledge that the interrupt was received
+	EALLOW;
+	PieCtrlRegs.PIEACK.bit.ACK5 = PIEACK_GROUP7;
+	EDIS;
+}
+
+// move this code to RAM
+#pragma CODE_SECTION(dma_ch6_int_isr, ".TI.ramfunc");
+__interrupt void dma_ch6_int_isr(void)
+{
+	// acknowledge that the interrupt was received
+	EALLOW;
+	PieCtrlRegs.PIEACK.bit.ACK6 = PIEACK_GROUP7;
+	EDIS;
+}
+
 void configure_adcs(void)
 {
 	// allow writing to protected registers
@@ -52,16 +96,20 @@ void configure_adcs(void)
 
 	// set the ADC clocks to one quarter of the input clock
 	AdcaRegs.ADCCTL2.bit.PRESCALE = 0x6;
+	AdcbRegs.ADCCTL2.bit.PRESCALE = 0x6;
 
 	// set the ADCs to have twelve bit resolution and put it in single signal mode
 	// in single signal mode, the input pin voltage is referenced against VREFLO
 	AdcSetMode(ADC_ADCA, ADC_RESOLUTION_12BIT, ADC_SIGNALMODE_SINGLE);
+	AdcSetMode(ADC_ADCB, ADC_RESOLUTION_12BIT, ADC_SIGNALMODE_SINGLE);
 
 	// generate interrupt pulses at the end of conversions
 	AdcaRegs.ADCCTL1.bit.INTPULSEPOS = 1;
+	AdcbRegs.ADCCTL1.bit.INTPULSEPOS = 1;
 
 	// power up the ADCs
 	AdcaRegs.ADCCTL1.bit.ADCPWDNZ = 1;
+	AdcbRegs.ADCCTL1.bit.ADCPWDNZ = 1;
 
 	// give time for the ADCs to power up
 	DELAY_US(ADC_WARMUP_TIME);
@@ -76,28 +124,64 @@ void setup_adc_channels(void)
 	EALLOW;
 
 	// set channels
-	AdcaRegs.ADCSOC0CTL.bit.CHSEL = 2; //ADCINA2
-	AdcaRegs.ADCSOC1CTL.bit.CHSEL = 3; //ADCINA3
+	AdcaRegs.ADCSOC0CTL.bit.CHSEL = 2; //adcin a2
+	AdcaRegs.ADCSOC1CTL.bit.CHSEL = 3; //adcin a3
+	AdcaRegs.ADCSOC2CTL.bit.CHSEL = 4; //adcin a4
+	AdcaRegs.ADCSOC3CTL.bit.CHSEL = 5; //adcin a5
+	AdcbRegs.ADCSOC0CTL.bit.CHSEL = 2; //adcin b2
+	AdcbRegs.ADCSOC1CTL.bit.CHSEL = 3; //adcin b3
+	AdcbRegs.ADCSOC2CTL.bit.CHSEL = 4; //adcin b4
+	AdcbRegs.ADCSOC3CTL.bit.CHSEL = 5; //adcin b5
 
 	// set sample windows
 	AdcaRegs.ADCSOC0CTL.bit.ACQPS = ACQPS_SETTING;
 	AdcaRegs.ADCSOC1CTL.bit.ACQPS = ACQPS_SETTING;
+	AdcaRegs.ADCSOC2CTL.bit.ACQPS = ACQPS_SETTING;
+	AdcaRegs.ADCSOC3CTL.bit.ACQPS = ACQPS_SETTING;
+	AdcbRegs.ADCSOC0CTL.bit.ACQPS = ACQPS_SETTING;
+	AdcbRegs.ADCSOC1CTL.bit.ACQPS = ACQPS_SETTING;
+	AdcbRegs.ADCSOC2CTL.bit.ACQPS = ACQPS_SETTING;
+	AdcbRegs.ADCSOC3CTL.bit.ACQPS = ACQPS_SETTING;
 
 	// set what SOCs trigger ADC interrupt registers
 	AdcaRegs.ADCINTSEL1N2.bit.INT1SEL = 0;
 	AdcaRegs.ADCINTSEL1N2.bit.INT2SEL = 1;
+	AdcaRegs.ADCINTSEL3N4.bit.INT3SEL = 2;
+	AdcaRegs.ADCINTSEL3N4.bit.INT4SEL = 3;
+	AdcbRegs.ADCINTSEL1N2.bit.INT1SEL = 0;
+	AdcbRegs.ADCINTSEL1N2.bit.INT2SEL = 1;
+	AdcbRegs.ADCINTSEL3N4.bit.INT3SEL = 2;
+	AdcbRegs.ADCINTSEL3N4.bit.INT4SEL = 3;
 
-	// enable the first two ADC interrupt registers
+	// enable the ADC interrupt registers
 	AdcaRegs.ADCINTSEL1N2.bit.INT1E = 1;
 	AdcaRegs.ADCINTSEL1N2.bit.INT2E = 1;
+	AdcaRegs.ADCINTSEL3N4.bit.INT3E = 1;
+	AdcaRegs.ADCINTSEL3N4.bit.INT4E = 1;
+	AdcbRegs.ADCINTSEL1N2.bit.INT1E = 1;
+	AdcbRegs.ADCINTSEL1N2.bit.INT2E = 1;
+	AdcbRegs.ADCINTSEL3N4.bit.INT3E = 1;
+	AdcbRegs.ADCINTSEL3N4.bit.INT4E = 1;
 
-	// enable continuous mode for the first two ADC interrupt registers
+	// enable continuous mode for the ADC interrupt registers
 	AdcaRegs.ADCINTSEL1N2.bit.INT1CONT = 1;
 	AdcaRegs.ADCINTSEL1N2.bit.INT2CONT = 1;
+	AdcaRegs.ADCINTSEL3N4.bit.INT3CONT = 1;
+	AdcaRegs.ADCINTSEL3N4.bit.INT4CONT = 1;
+	AdcbRegs.ADCINTSEL1N2.bit.INT1CONT = 1;
+	AdcbRegs.ADCINTSEL1N2.bit.INT2CONT = 1;
+	AdcbRegs.ADCINTSEL3N4.bit.INT3CONT = 1;
+	AdcbRegs.ADCINTSEL3N4.bit.INT4CONT = 1;
 
-    // reset the first two ADC interrupt registers
+    // reset the ADC interrupt registers
     AdcaRegs.ADCINTFLGCLR.bit.ADCINT1 = 1;
     AdcaRegs.ADCINTFLGCLR.bit.ADCINT2 = 1;
+    AdcaRegs.ADCINTFLGCLR.bit.ADCINT3 = 1;
+    AdcaRegs.ADCINTFLGCLR.bit.ADCINT4 = 1;
+    AdcbRegs.ADCINTFLGCLR.bit.ADCINT1 = 1;
+    AdcbRegs.ADCINTFLGCLR.bit.ADCINT2 = 1;
+    AdcbRegs.ADCINTFLGCLR.bit.ADCINT3 = 1;
+    AdcbRegs.ADCINTFLGCLR.bit.ADCINT4 = 1;
 
 	// disallow writing to protected registers
 	EDIS;
@@ -140,8 +224,22 @@ void configure_dma(void)
 	// initialize DMA
 	DMAInitialize();
 
+	// setup DMA channel one
+	DMACH1AddrConfig((volatile Uint16 *)(&analog_ins[0][0]), (volatile Uint16 *)(&AdcaResultRegs.ADCRESULT0));
+
+	// send one 16 bit word in a burst
+	DMACH1BurstConfig(1, 0, 0);
+
+	// send one burst per transfer
+	DMACH1TransferConfig(1, 0, 0);
+
+	// configure DMA channel one
+	DMACH1ModeConfig(1, PERINT_ENABLE, ONESHOT_DISABLE, CONT_ENABLE,
+					 SYNC_DISABLE, SYNC_SRC, OVRFLOW_DISABLE, SIXTEEN_BIT,
+					 CHINT_END, CHINT_ENABLE);
+
 	// setup DMA channel two
-	DMACH2AddrConfig((volatile Uint16 *)(&analog_ins[0][0]), (volatile Uint16 *)(&AdcaResultRegs.ADCRESULT0));
+	DMACH2AddrConfig((volatile Uint16 *)(&analog_ins[0][1]), (volatile Uint16 *)(&AdcaResultRegs.ADCRESULT1));
 
 	// send one 16 bit word in a burst
 	DMACH2BurstConfig(1, 0, 0);
@@ -150,12 +248,12 @@ void configure_dma(void)
 	DMACH2TransferConfig(1, 0, 0);
 
 	// configure DMA channel two
-	DMACH2ModeConfig(1, PERINT_ENABLE, ONESHOT_DISABLE, CONT_ENABLE,
+	DMACH2ModeConfig(2, PERINT_ENABLE, ONESHOT_DISABLE, CONT_ENABLE,
 					 SYNC_DISABLE, SYNC_SRC, OVRFLOW_DISABLE, SIXTEEN_BIT,
 					 CHINT_END, CHINT_ENABLE);
 
 	// setup DMA channel three
-	DMACH3AddrConfig((volatile Uint16 *)(&analog_ins[0][1]), (volatile Uint16 *)(&AdcaResultRegs.ADCRESULT1));
+	DMACH3AddrConfig((volatile Uint16 *)(&kp), (volatile Uint16 *)(&AdcaResultRegs.ADCRESULT2));
 
 	// send one 16 bit word in a burst
 	DMACH3BurstConfig(1, 0, 0);
@@ -164,7 +262,7 @@ void configure_dma(void)
 	DMACH3TransferConfig(1, 0, 0);
 
 	// configure DMA channel three
-	DMACH3ModeConfig(2, PERINT_ENABLE, ONESHOT_DISABLE, CONT_ENABLE,
+	DMACH3ModeConfig(3, PERINT_ENABLE, ONESHOT_DISABLE, CONT_ENABLE,
 					 SYNC_DISABLE, SYNC_SRC, OVRFLOW_DISABLE, SIXTEEN_BIT,
 					 CHINT_END, CHINT_ENABLE);
 }
@@ -179,25 +277,34 @@ void pid_loop(int16 errors[NUM_OUTPUTS],
 	// start the ADC SOCs
 	AdcaRegs.ADCSOCFRC1.bit.SOC0 = 1;
 	AdcaRegs.ADCSOCFRC1.bit.SOC1 = 1;
+	AdcaRegs.ADCSOCFRC1.bit.SOC2 = 1;
+	AdcaRegs.ADCSOCFRC1.bit.SOC3 = 1;
+	AdcbRegs.ADCSOCFRC1.bit.SOC0 = 1;
+	AdcbRegs.ADCSOCFRC1.bit.SOC1 = 1;
+	AdcbRegs.ADCSOCFRC1.bit.SOC2 = 1;
+	AdcbRegs.ADCSOCFRC1.bit.SOC3 = 1;
 
 	// perform negative PID on all outputs
 	size_t i;
+	long pout;
 	for (i = 0; i < NUM_OUTPUTS; i++) {
 		errors[i] = analog_ins[i][1] - analog_ins[i][0];
 		integrals[i] += errors[i] * DT;
 		derivatives[i] = (errors[i] - previous_errors[i]) / DT;
-		analog_outs[i] = MIN(KP * errors[i] + KI * integrals[i] + KD * derivatives[i] + DAC_OFFSET_VALUE, DAC_MAX_VALUE - 1);
+		pout = (long) errors[i] * AdcaResultRegs.ADCRESULT2 / 3000;
+		// printf("%d\n", pout);
+		analog_outs[i] = MIN(pout + KI * integrals[i] + KD * derivatives[i] + DAC_OFFSET_VALUE, DAC_MAX_VALUE - 1);
 		previous_errors[i] = errors[i];
 	}
-	// printf("%d %d \n", errors[0], integrals[0]);
+	// printf("%d %d %d\n", analog_ins[0][0], kp, AdcaResultRegs.ADCRESULT2);
+	// printf("%d %d\n", errors[0], AdcaResultRegs.ADCRESULT2);
 
 	// output on the DACs if the hold signal is off
 	if (GPIO_ReadPin(HOLD_OUTPUT_GPIO_PIN) == 0) {
 		DacaRegs.DACVALS.bit.DACVALS = DAC_OFFSET_VALUE; //Adcsoc0 // adcina2 //
-		DacbRegs.DACVALS.bit.DACVALS = analog_outs[0];
-		DaccRegs.DACVALS.bit.DACVALS = MIN(errors[0] + DAC_OFFSET_VALUE, DAC_MAX_VALUE - 1);
+		DacbRegs.DACVALS.bit.DACVALS = pout;
+		DaccRegs.DACVALS.bit.DACVALS = errors[0];
 	}
-
 
 	// turn on LED if locked
 	if (errors[0] < LED_THRESHOLD)
@@ -243,8 +350,12 @@ int main(void)
 	PieCtrlRegs.PIECTRL.bit.ENPIE = 1;
 
 	// enable DMA interrupts
+	PieCtrlRegs.PIEIER7.bit.INTx1 = 1;
 	PieCtrlRegs.PIEIER7.bit.INTx2 = 1;
 	PieCtrlRegs.PIEIER7.bit.INTx3 = 1;
+	PieCtrlRegs.PIEIER7.bit.INTx4 = 1;
+	PieCtrlRegs.PIEIER7.bit.INTx5 = 1;
+	PieCtrlRegs.PIEIER7.bit.INTx6 = 1;
 	IER = M_INT7;
 
 	// enable global interrupts
@@ -255,8 +366,12 @@ int main(void)
 
 	// map DMA interrupts to functions
 	EALLOW;
+	PieVectTable.DMA_CH1_INT= &dma_ch1_int_isr;
 	PieVectTable.DMA_CH2_INT= &dma_ch2_int_isr;
 	PieVectTable.DMA_CH3_INT= &dma_ch3_int_isr;
+	PieVectTable.DMA_CH4_INT= &dma_ch4_int_isr;
+	PieVectTable.DMA_CH5_INT= &dma_ch5_int_isr;
+	PieVectTable.DMA_CH6_INT= &dma_ch6_int_isr;
 	EDIS;
 
 	// copy the necessary code from flash memory to RAM
@@ -293,6 +408,7 @@ int main(void)
 	}
 
 	// start the DMA channels
+	StartDMACH1();
 	StartDMACH2();
 	StartDMACH3();
 
